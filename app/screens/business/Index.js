50952792603289dfb078258d4d4212c567c5e759
Mohammad Meftahi <meftahee@gmail.com>
2018-09-01 20:17:08 +0430

    Add Details Tab Content to business screen

--- app/screens/business/Index.js
+++ app/screens/business/Index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Text, TouchableOpacity, Image, Dimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+// my components
 import TopIcons from './TopIcons';
 import MainProperties from './MainProperties';
 import OtherProperties from './OtherProperties';
@@ -11,8 +13,107 @@ class Business extends Component {
 		this.state = {};
 	}
 
+	detailsTabContent = () => {
+		screenWidth = Dimensions.get('window').width;
+		let tabContentStyles = {
+			addressContainer: {
+				flexDirection: 'row',
+				justifyContent: 'space-between',
+				borderTopWidth: 1,
+				borderBottomWidth: 1,
+				paddingVertical: 5,
+				borderColor: '#ccc',
+				alignItems: 'center',
+				paddingRight: 10
+			},
+			addressButton: {
+				marginTop: 20,
+				marginBottom: 10,
+				paddingLeft: 10
+			}
+		};
+
+		return (
+			<View>
+				<TouchableOpacity style={tabContentStyles.addressButton}>
+					<View style={tabContentStyles.addressContainer}>
+						<Text>
+							<Text style={{ fontWeight: 'bold', color: '#000' }}>Address: </Text>
+							<Text style={{ color: '#333', fontSize: 12 }}>1120 N Street Sacaramento</Text>
+						</Text>
+						<Icon size={18} name="angle-right" />
+					</View>
+				</TouchableOpacity>
+
+				<View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
+					<TouchableOpacity
+						style={{
+							justifyContent: 'center',
+							alignItems: 'center',
+							backgroundColor: '#aaa',
+							padding: 3,
+							borderRadius: 5,
+							minWidth: '25%'
+						}}
+					>
+						<View style={{}}>
+							<Icon size={18} name="angle-right" />
+							<Text>Call</Text>
+						</View>
+					</TouchableOpacity>
+
+					<TouchableOpacity
+						style={{
+							justifyContent: 'center',
+							alignItems: 'center',
+							backgroundColor: '#aaa',
+							padding: 3,
+							borderRadius: 5,
+							minWidth: '25%'
+						}}
+					>
+						<View style={{}}>
+							<Icon size={18} name="angle-right" />
+							<Text>Email</Text>
+						</View>
+					</TouchableOpacity>
+
+					<TouchableOpacity
+						style={{
+							justifyContent: 'center',
+							alignItems: 'center',
+							backgroundColor: '#aaa',
+							padding: 3,
+							borderRadius: 5,
+							minWidth: '25%'
+						}}
+					>
+						<View>
+							<Icon size={18} name="angle-right" />
+							<Text>Website</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
+
+				<ScrollView horizontal contentContainerStyle={{marginTop: 10}}>
+					<Image
+						style={{ width: screenWidth /2.4, borderRadius: 5, resizeMode: 'cover', marginLeft: 10 }}
+						source={require('../../assets/images/love.jpg')}
+					/>
+					<Image
+						style={{ width: screenWidth /2.4, borderRadius: 5, resizeMode: 'cover', marginLeft: 10 }}
+						source={require('../../assets/images/love.jpg')}
+					/>
+					<Image
+						style={{ width: screenWidth /2.4, borderRadius: 5, resizeMode: 'cover', marginLeft: 10 }}
+						source={require('../../assets/images/love.jpg')}
+					/>
+				</ScrollView>
+			</View>
+		);
+	};
+
 	render() {
-		
 		return (
 			<View style={styles.container}>
 				<ScrollView showsVerticalScrollIndicator={false}>
@@ -20,11 +121,11 @@ class Business extends Component {
 
 					<MainProperties />
 
-                   
-                    <OtherProperties/>
+					<OtherProperties />
 
-                     <TabBarButtons/>
+					<TabBarButtons />
 
+					{this.detailsTabContent()}
 				</ScrollView>
 			</View>
 		);
